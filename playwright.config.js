d657bb67856bb60339670f4e0d0cdbb2c4803161
vdespa-jenkins <148623005+vdespa-jenkins@users.noreply.github.com>
2023-11-06 14:55:34 +0000

    Re-configure

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
-    ['junit', { outputFile: 'test-results-e2e/results.xml' }]
+    ['junit']
   ],  
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
